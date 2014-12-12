9d756247f42ee9c39a9b9e098b38aa3e5b2208c6
Andrew Kidoo <github@kustomzone.com>
2014-12-12 16:54:05 +1300

    Rename

--- droid.js
+++ droid.js
@@ -17965,15 +17965,10 @@ VedApp.prototype.renderDirectory = function(appDiv, mode) {
   centerDiv.className = 'center';
   centerDiv.innerHTML +=
       '<div class=center>' +
-      '<h1>What?</h1>' +
-      'Vorp is a free 2D physics-based action/puzzle game that runs in modern web browsers.' +
+      '<h1>Droid</h1>' +
+      'Droid is a free 2D physics-based action/puzzle game that runs in modern web browsers.' +
       '<p>It\'s written in JavaScript, using the HTML5 &lt;canvas&gt; element to render all the graphics. ' +
       'There are few sound effects, the graphics are terrible, and you need a keyboard to play.' +
-      '<p>It works well in <a href="http://www.google.com/chrome/">Chrome</a>, ' +
-      '<a href="http://www.mozilla.org/firefox">Firefox</a>, ' +
-      '<a href="http://www.apple.com/safari/">Safari</a>, and ' +
-      '<a href="http://www.opera.com/browser">Opera</a>.<br>' +
-      '<a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home">IE 9</a> and up might work.' +
       '<p>The built-in editor stores your work on your computer using localstorage. ' +
       'You can share what you make, by sending a URL.' +
       '<h1>Official Levels</h1>' +
